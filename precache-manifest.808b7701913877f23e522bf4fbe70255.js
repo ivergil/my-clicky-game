63823862e130d83b97f704404e6a202c33b0742c
IDGB <49776938+ivergil@users.noreply.github.com>
2019-09-29 21:12:30 -0600

    Updates

---
+++ precache-manifest.808b7701913877f23e522bf4fbe70255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f913bb3adf3d5f9af3e8edae055b1e8",
+    "url": "/my-clicky-game/index.html"
+  },
+  {
+    "revision": "3dd8ad4f9b87e402852a",
+    "url": "/my-clicky-game/static/css/main.16f4efce.chunk.css"
+  },
+  {
+    "revision": "42a03cb2666060362550",
+    "url": "/my-clicky-game/static/js/2.2e2e2d98.chunk.js"
+  },
+  {
+    "revision": "3dd8ad4f9b87e402852a",
+    "url": "/my-clicky-game/static/js/main.4e760045.chunk.js"
+  },
+  {
+    "revision": "4d2ee251913fde49c1dd",
+    "url": "/my-clicky-game/static/js/runtime-main.eee7a266.js"
+  }
+]);
